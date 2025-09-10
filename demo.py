ff78f582df573bdfccceec080674e9f0fa18f0f6
JPOrellana <orellanaorellanapablo@gmail.com>
2025-09-10 17:02:03 -0600

    Creación Demo

---
+++ demo.py
@@ -0,0 +1,7 @@
+from funciones import square, factorial, is_prime, gcd, lcm
+
+print("square(5) =", square(5))
+print("factorial(5) =", factorial(5))
+print("is_prime(13) =", is_prime(13))
+print("gcd(12, 18) =", gcd(12, 18))
+print("lcm(4, 6) =", lcm(4, 6))
